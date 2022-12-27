13841b5a9330f8313291316744bbc790b7fdba1f
AliMusa04 <tu75nx176@code.edu.az>
2022-12-27 11:47:12 +0400

    first commit

---
+++ WebApplication3/Program.cs
@@ -0,0 +1,15 @@
+namespace WebApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseRouting();
+            app.MapControllerRoute(name:"default", pattern:"{controller=home}/{action}");
+            app.Run();
+        }
+    }
+}
